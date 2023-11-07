3af17abd653ae65ebd527571950a3d6b6e08b607
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2023-11-07 12:56:17 -0300

    feat(lessons): Add JavaScript data types
    Created a series of JavaScript data types. This file displays various JavaScript data types, including numbers, strings, booleans, objects, and more.

---
+++ JavaScript-Origamid/02-tiposDeDados/script.js
@@ -0,0 +1,45 @@
+// Tipo de dados
+var team = 'Flamengo'; // String
+var age = 128; // Number
+var haveStadium = true; // Boolean
+var rival; // Undefined
+var trainer = null; // Null
+var symbol = Symbol() // Symbol
+var newObject = {} // Object
+
+
+// Verificando o tipo
+console.log(typeof(age));
+// OBS: typeof null retorna object
+
+
+// Concatenação de variáveis
+const firstName = 'Bruno';
+const lastName = 'Silva';
+let ageCurrent = 21;
+let goals = 1000;
+
+
+let fullName = firstName + ' ' + lastName;
+let txtConc = `Meu nome é ${fullName} e eu tenho ${ageCurrent}`;
+let pele = 'Pele fez mais de ' + goals + ' gols!';
+
+
+// Exercícios
+// Declare uma variável contendo uma string
+let color = 'Azul';
+
+// Declare uma variável contendo um número dentro de uma string
+let year = '2001';
+
+// Declare uma variável com a sua idade
+let myAge = 21;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+let myName = 'Haroldo', myLastName = 'Mayworm', MyFullName = myName + myLastName;
+
+// Coloque a seguinte frase em uma variável: It's time
+let txt = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof(myName));
