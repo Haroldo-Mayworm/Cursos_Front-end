0e5b95c8340a6084ad999379e59ab7fe1742b924
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2023-11-07 12:16:42 -0300

    feat(lessons): Add JavaScript variable
    Created a series of JavaScript variables. This file shows some ways to declare variables.

---
+++ JavaScript-Origamid/01-variaveis/script.js
@@ -0,0 +1,38 @@
+const nome = 'Haroldo';
+const sobrenome = 'Mayworm';
+let idade = 21;
+
+console.log(nome, sobrenome, idade);
+
+
+const peso = 5;
+let quantidade = 8;
+
+console.log(peso * quantidade);
+
+
+let aluno = 'João', turma = 1, cidade = 'Rio de Janeiro';
+
+console.log(aluno, turma, cidade);
+
+
+let novoPreco;
+
+console.log(novoPreco);
+
+
+// Exercícios
+// Declarar uma variável com o seu nome
+const meuNome = 'Haroldo mayworm';
+
+// Declarar uma variável com a sua idade
+let minhaIdade = 21;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var preco, cor, tamanho, material, possuiGarantia;
