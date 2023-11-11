88f14bdf1dd3ea0b194fd63d6969021df1f3269c
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2023-11-11 17:48:23 -0300

    feat(lessons): Add JavaScript ternary operators and assignment
    created a file on ternary operators and assignment operators.

---
+++ JavaScript-Origamid/08-operadoresAtribuiçãoTernarios/script.js
@@ -0,0 +1,16 @@
+// Atribuição
+let x = 5;
+let y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+
+// Ternário
+let age = 19;
+let canDrink = (age >= 18) ? 'Pode beber' : 'Não pode beber';
+// condição ? true : false
+console.log(canDrink) // Pode beber
